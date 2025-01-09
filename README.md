32e9a523a45fda6511b4a3924c8f38c008bd4bbe
gaspar <gaspar@pizzati.com>
2025-01-09 11:14:40 -0600

    Filtro gobal

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 * http://localhost:9090/
 * http://localhost:8761/
 
-#Para correr con puertos aleatoreos
+# Para correr con puertos aleatoreos
 * abrir nueva ventana desde el servicio a replicar
 * ejecutar ./mvnw spring-boot:run
+
+# Liberar puerto 
+* netstat -ano | grep 8090
+* primer consulta el puerto que se decea, aqui el 8090, luego con el id, se libera en el admin de tareas
--- gateway-server/src/main/java/com/gaspar/gateway_server/filters/SampleGlobalFilter.java
+++ gateway-server/src/main/java/com/gaspar/gateway_server/filters/SampleGlobalFilter.java
@@ -1,9 +1,12 @@
 package com.gaspar.gateway_server.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,19 +15,39 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleGlobalFilter implements GlobalFilter{
+public class SampleGlobalFilter implements GlobalFilter, Ordered{
 
     private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info("Ejecutando filtro antes de request");        
+        logger.info("Ejecutando filtro antes de request");     
+        
+        exchange.getRequest().mutate()
+        .headers(h->h.add("token", "patito"));
+
         return chain.filter(exchange)
             .then(Mono.fromRunnable(()->{
                 logger.info("ejecutando filtro post");
+                String token = exchange.getRequest().getHeaders().get("token").get(0);
+                logger.info("token:" +  token);
+
+                Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token"))
+                .ifPresent(
+                    tvalue->{
+                        logger.info("token Optional:" +  tvalue);
+                        exchange.getResponse().getHeaders().add("token", tvalue);
+                    }
+                );
+
                 exchange.getResponse().getCookies().add("color", ResponseCookie.from("color","red").build());
                 exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
             }));
+    }
+
+    @Override
+    public int getOrder() {        
+        return 100;
     }
 
 }
